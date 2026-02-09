3fd6eea6c19663b3365cf317ceeb44ccd373536e
luca.perez <luca.perez@studenti.unimi.it>
2026-02-09 12:45:47 +0100

    Passed test for And formula

--- app/src/main/java/mathematicallogic/formula/And.java
+++ app/src/main/java/mathematicallogic/formula/And.java
@@ -1,5 +1,10 @@
 package src.main.java.mathematicallogic.formula;
 
 public class And implements Formula {
-    public And(Formula a, Formula b) {}
+    private final Formula f1 ;
+    private final Formula f2 ;
+
+    public And(Formula a, Formula b) { this.f1 = a ; this.f2 = b ; }
+
+    @Override public String toString() { return "(" + f1 + " ∧ " + f2 + ")" ; }
 }
