2ec424e524b699967dd3ee4ea6d4524fb089121f
luca.perez <luca.perez@studenti.unimi.it>
2026-02-09 17:15:46 +0100

    Created test for simple BDD constructor

---
+++ app/src/main/java/mathematicallogic/bdd/BDDNode.java
@@ -0,0 +1,11 @@
+package src.main.java.mathematicallogic.bdd;
+
+public class BDDNode {
+    private final boolean value ;
+
+    public BDDNode(boolean value) { this.value = value ; }
+
+    public boolean getValue() { return false ; }
+
+
+}
---
+++ app/src/test/java/mathematicallogic/bdd/BDDTest.java
@@ -0,0 +1,19 @@
+package src.test.java.mathematicallogic.bdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+import src.main.java.mathematicallogic.bdd.BDDNode;
+import src.main.java.mathematicallogic.formula.Var;
+
+public class BDDTest {
+
+    @Test
+    public void testBuildSimpleLeafBDD() {
+        BDDNode a = new BDDNode(true) ;
+        BDDNode b = new BDDNode(false) ;
+
+        Assert.assertEquals(true, a.getValue()) ;
+        Assert.assertEquals(false, b.getValue());
+    }
+
+}
